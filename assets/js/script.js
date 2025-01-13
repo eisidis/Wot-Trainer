ed44f1689eaeb5e6aed58dc8b4b421826aaf61ed
eisidis <julien023001@gmail.com>
2025-01-13 01:26:19 +0100

    affiche random tank dans la console

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,17 @@ validate.addEventListener("click", () => {
 fetch('https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id=77b55a76eb843a2b51927278550b8534&tier=10&nation=usa&type=heavyTank')
     .then(response => response.json()) // Convertir la réponse en JSON
     .then(data => {
-        const tankData = data.data // Convertir l'objet en tableau de valeurs
+        const tankData = Object.values(data.data) // Convertir l'objet en tableau de valeurs
+        // const tankArray = Object.values(tankData); inutile
 
-        console.log(tankData["14881"]["name"]);
+        // console.log(tankData["14881"]["name"]);
 
         nextBtn.addEventListener("click", () => {
             // console.log(Object.keys(data));
-            var randomIndex = Math.floor(Math.random() * tankData.length);
-            console.log(tankData);
+            var randomIndex = Math.floor(Math.random() * tankData.length); // Génère un nombre aléatoire en fonction de la taille du tableau
+            // console.log(randomIndex);
+            console.log(tankData[randomIndex]["name"]);
+            
             // console.log(Object.keys(data.data)[random]);
         })
     })
