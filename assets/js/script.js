65afad53006a5073ad6a159706784340467b0a37
eisidis <julien023001@gmail.com>
2025-01-14 20:05:55 +0100

    reponse HP fonctionnel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,40 @@
 const nextBtn = document.getElementById("next")
-validate.addEventListener("click", () => {
-    console.log("ahah t'es pas beau");
+const tankName = document.getElementById("tankName")
+const userHp = document.getElementById("userHp")
+const resultHp = document.getElementById("resultHp")
 
-})
 
 fetch('https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id=77b55a76eb843a2b51927278550b8534&tier=10&nation=usa&type=heavyTank')
     .then(response => response.json()) // Convertir la réponse en JSON
     .then(data => {
         const tankData = Object.values(data.data) // Convertir l'objet en tableau de valeurs
-        // const tankArray = Object.values(tankData); inutile
 
         // console.log(tankData["14881"]["name"]);
+        // console.log(Object.keys(data));
+        var randomIndex = Math.floor(Math.random() * tankData.length); // Génère un nombre aléatoire en fonction de la taille du tableau
+        // console.log(randomIndex);
+        console.log(tankData[randomIndex]["name"]);
 
+        const title = tankName.appendChild(document.createElement("p"));
+        title.innerHTML = tankData[randomIndex]["name"];
+
+        tankName.appendChild(document.createElement("img")).src = tankData[randomIndex]["images"]["big_icon"];
+        // console.log(Object.keys(data.data)[random]);
         nextBtn.addEventListener("click", () => {
-            // console.log(Object.keys(data));
-            var randomIndex = Math.floor(Math.random() * tankData.length); // Génère un nombre aléatoire en fonction de la taille du tableau
-            // console.log(randomIndex);
-            console.log(tankData[randomIndex]["name"]);
-            
-            // console.log(Object.keys(data.data)[random]);
+            location.reload()
         })
+        validate.addEventListener("click", () => {
+            const tank = tankData[randomIndex]["default_profile"];
+            
+            if (userHp.value == tank["hp"]) {
+                console.log("Bravo");
+                resultHp.innerText = "Bravo";
+            } else {
+                console.log("Perdu");
+                resultHp.innerText = "raté big noob ahahah";
+            }
+        })
+
     })
     .catch(error => {
         // Gérer les erreurs
